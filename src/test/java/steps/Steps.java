f58027e0ca91fcf8f39607e9d1ef5fec178cdc8b
Belan.N.V <n.v.Belan@mail.ru>
2020-09-29 13:31:02 +0300

    1

--- src/test/java/steps/Steps.java
+++ src/test/java/steps/Steps.java
@@ -10,36 +10,45 @@ public class Steps {
 
     @Когда("^Выбираем меню Ипотека$")
     public void step1() {
+        System.out.println(1);
         app.getStartPage().selectMenuIpoteka();
     }
 
     @Когда("Выбираем подменю Ипотека на готовое жильё")
     public void step2() {
+
+        System.out.println(2);
         app.getStartPage().selectSecMenu();
     }
 
     @Тогда("Проверяем переход на страницу")
     public void step3() {
+        System.out.println(3);
         app.getIpotekaPage().checkTitle();
     }
 
     @Когда("Нажимаем кнопку Подать заявку")
     public void step4() {
+
+        System.out.println(4);
         app.getIpotekaPage().pushButtonZayavka();
     }
 
     @Тогда("Проверяем заголовок Выберите программу и рассчитайте условия")
     public void step5() {
+
         app.getZayavkaPage().checkTitle();
     }
 
     @Тогда("Проверяем цель кредита")
     public void step6() {
+        System.out.println(5);
         app.getZayavkaPage().checkAndSetLoanPurpose();
     }
 
     @Когда("Проверяем и устанавливаем переключатели")
     public void step7(DataTable dataTable) {
+        System.out.println(6);
         dataTable.cells().forEach(raw -> {
                     app.getZayavkaPage().setSwitch(raw.get(0), raw.get(1));
                 }
@@ -48,6 +57,7 @@ public class Steps {
 
     @Когда("Заполняем поля")
     public void step8(DataTable dataTable) {
+        System.out.println(7);
         dataTable.cells().forEach(raw -> {
                     app.getZayavkaPage().fillValue(raw.get(0), raw.get(1));
                 }
